32177cb783903235ebceb60e491c6bd913f698f9
Jennings Anderson <jennings.anderson@colorado.edu>
2020-07-23 17:29:45 -0700

    Create RUN.sh

---
+++ RUN.sh
@@ -0,0 +1,15 @@
+./step-0-install-dependencies-and-download-planet.sh 
+
+./step-1-osmium-tags-filter.sh planet-latest.osm.pbf
+
+./step-2-convert-geometries.sh
+
+rm planet-latest.osm.pbf
+
+./step-3-tile.sh
+
+./step-4-download-qa-tiles.sh
+
+./step-5-tile-join.sh planet-latest.mbtiles
+
+rm planet-latest.mbtiles
