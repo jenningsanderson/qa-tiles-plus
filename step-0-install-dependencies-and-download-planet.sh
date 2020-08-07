4439bb02111d2e70a2699dc9d981b3e865eaa6a5
Jennings Anderson <jennings.anderson@gmail.com>
2020-08-07 22:41:23 +0000

    updating scripts

--- step-0-install-dependencies-and-download-planet.sh
+++ step-0-install-dependencies-and-download-planet.sh
@@ -8,5 +8,9 @@ echo "Installing tippecanoe"
 mason/mason install tippecanoe 1.32.10
 mason/mason link tippecanoe 1.32.10
 
+echo "Installing osmium"
+mason/mason install osmium-tool 1.11.0
+mason/mason link osmium-tool 1.11.0
+
 echo "Downloading planet-latest.osm.pbf from a US mirror"
 wget https://ftpmirror.your.org/pub/openstreetmap/pbf/planet-latest.osm.pbf
--- step-1-osmium-tags-filter.sh
+++ step-1-osmium-tags-filter.sh
@@ -1,1 +1,1 @@
-~/osm-wayback/mason_packages/.link/bin/osmium tags-filter --overwrite --verbose $1 r/type=restriction -o restrictions.osm.pbf
+mason_packages/.link/bin/osmium tags-filter --overwrite --verbose $1 r/type=restriction -o restrictions.osm.pbf
